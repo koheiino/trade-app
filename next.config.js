3509abe5776401c691066768a64ee9c9a0f224e2
Kohei higashi <kohei503310@gmail.com>
2025-06-05 23:02:27 +0900

    fix: vercelエラー修正

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  // その他の設定
+};
+
+module.exports = nextConfig;
